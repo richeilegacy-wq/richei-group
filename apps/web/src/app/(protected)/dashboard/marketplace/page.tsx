db948c9da187f5180230cea89381b75a55a89cc2
justuche224 <justuche224@gmail.com>
2026-02-13 11:15:46 +0100

    add coming soon to market place

--- apps/web/src/app/(protected)/dashboard/marketplace/page.tsx
+++ apps/web/src/app/(protected)/dashboard/marketplace/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="w-full h-svh flex items-center justify-center">
+      <h1 className="text-5xl">Coming Soon...</h1>
+    </div>
+  );
+};
 
-export default page
+export default page;
